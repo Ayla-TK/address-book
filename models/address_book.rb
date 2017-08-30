50c2c1209672b31f30599a7b00799993134531d0
Ayla Tolosa-Kline <AylaMPH@gmail.com>
2017-08-30 15:50:57 +0300

    Checkpoint 6 |AddressBloc-CSV: COMPLETE

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,10 +1,24 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
   attr_reader :entries
 
   def initialize
      @entries = []
+   end
+
+   def remove_entry(name, phone_number, email)
+     erase_entry = nil
+
+     @entries.each do |entry|
+
+       if name == entry.name && phone_number == entry.phone_number && email == entry.email
+         erase_entry = entry
+       end
+     end
+
+     @entry.erase(erase_entry)
    end
 
   def add_entry(name, phone_number, email)
@@ -16,5 +30,14 @@ class AddressBook
       index += 1
     end
     entries.insert(index, Entry.new(name, phone_number,email))
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
   end
 end
